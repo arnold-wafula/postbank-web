fdd8423c3c4ae73a5f7e840b3af8c40a64bfddf4
Arnold Wafula <arnold.wafula0@gmail.com>
2025-09-24 11:05:17 +0300

    Changed from <a> to <Link>

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return(
         <nav className="flex justify-between items-center px-8 py-4 bg-white shadow">
             <div className="font-bold text-xl text-blue-700">Postbank</div>
             <ul className="flex gap-6 text-gray-700">
-                <li><a href="/">Home</a></li>
-                <li><a href="/services">Services</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/services">Services</Link></li>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
             </ul>
         </nav>
     )
