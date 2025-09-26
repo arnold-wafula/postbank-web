bc13a0e6d9707a5c8312c3b0d294ce3fbbd7411d
Arnold Wafula <arnold.wafula0@gmail.com>
2025-09-26 12:29:26 +0300

    Created contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+      <main className="px-8 py-16">
+        <h1 className="text-3xl font-bold text-blue-700 mb-6">Contact</h1>
+      </main>
+    );
+}
