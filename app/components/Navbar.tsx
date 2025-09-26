80e079e55b85053452e48035f90cb4e58b083a5e
Arnold Wafula <arnold.wafula0@gmail.com>
2025-09-26 12:29:14 +0300

    Small changes to Navbar padding

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,13 @@
+import Logo from "./Logo";
 import Link from "next/link";
 
 export default function Navbar() {
     return(
-        <nav className="flex justify-between items-center px-8 py-4 bg-white shadow">
-            <div className="font-bold text-xl text-blue-700">Postbank</div>
-            <ul className="flex gap-6 text-gray-700">
+        <nav className="flex justify-between items-center px-6 py-2 bg-white shadow">
+            <Link href="/">
+                <Logo />
+            </Link>
+            <ul className="flex gap-6 text-gray-700 text-sm sm:text-base">
                 <li><Link href="/">Home</Link></li>
                 <li><Link href="/services">Services</Link></li>
                 <li><Link href="/about">About</Link></li>
