67bed93370dd1ccb8aac64a5ed8d5d5ee20f7255
Arnold Wafula <arnold.wafula0@gmail.com>
2025-09-26 12:56:39 +0300

    Sizing logo changes

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,17 +1,16 @@
 import Image from "next/image";
 
 export default function Logo() {
-    return(
+    return (
         <div className="flex items-center">
-            <div className="relative w-auto h-10"> 
-                <Image
-                    src="/logo.png"
-                    alt="Postbank Logo"
-                    width={100}
-                    height={40}
-                    priority
-                />
-            </div>
+            <Image
+                src="/logo.png"
+                alt="Postbank Logo"
+                width={120}
+                height={50}
+                className="object-contain max-h-12 w-auto"
+                priority
+            />
         </div>
-    )
+    );
 }
