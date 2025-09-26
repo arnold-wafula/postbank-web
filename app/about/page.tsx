326977cc22412e2d20db3a49111b598de83fb6cd
Arnold Wafula <arnold.wafula0@gmail.com>
2025-09-26 12:28:19 +0300

    Created About page

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <main className="px-8 py-16">
+        <h1 className="text-3xl font-bold text-blue-700 mb-6">About Us</h1>
+      </main>
+    );
+}
