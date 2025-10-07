ea5a97fa101894a241dd8896a41beca07684208e
Arnold Wafula <arnold.wafula0@gmail.com>
2025-10-07 16:58:50 +0300

    added Services

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero"
+import Services from "./services/page"
+// import QuickAccess from "./components/QuickAccess"
 
 export default function Home() {
   return(
     <main className="min-h-screen bg-gray-50">
       <section className="text-center py-20">
         <Hero />
+        <Services />
+        {/* <QuickAccess /> */}
       </section>
     </main>
   )
