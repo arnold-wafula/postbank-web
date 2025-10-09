a5a985af3d9ba9f6e2c2a1ccb7a18623b25119bc
Arnold Wafula <arnold.wafula0@gmail.com>
2025-10-09 17:31:54 +0300

    Renamed the CTA class

---
+++ app/components/CTADownloadApp.tsx
@@ -0,0 +1,8 @@
+export default function CTADownloadApp() {
+    return(
+        <section className="relative bg-black text-white py-40 overflow-hidden">
+            <h2>Download the Postbank Mobile App</h2>
+            
+        </section>
+    )
+}
